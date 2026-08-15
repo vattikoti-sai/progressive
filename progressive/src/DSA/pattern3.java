fcf5334a4d26cd5e63a18ce2f0560dfbaf9f602e
vattikoti-sai <saivattikoti2@gmail.com>
2026-08-15 21:17:50 +0530

    committed on 15-08-26

--- progressive/src/DSA/pattern3.java
+++ progressive/src/DSA/pattern3.java
@@ -1,23 +1,27 @@
 package DSA;
 
 public class pattern3 {
-
-	public static void main(String[] args) {
-for(int i=1;i<=5;i++) {
-	for(int j=1;j<=i;j++) {
-		System.out.print("*");
-		if(j==5) {
-			System.out.println();
-			for(int k=4;k>=1;k--) {
-				for(int z=1;z<=k;z++) {
-					System.out.print("*");
+	void show(int n) {
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+				if(j==n) {
+					System.out.println();
+					for(int k=n-1;k>=1;k--) {
+						for(int z=1;z<=k;z++) {
+							System.out.print("*");
+						}
+						System.out.println();
+					}
 				}
-				System.out.println();
 			}
+			System.out.println();
 		}
 	}
-	System.out.println();
-}
+
+	public static void main(String[] args) {
+		pattern3 p= new pattern3();
+		p.show(7);
 	}
 
 }
